b75005afb56a31309bb87d4597d2e5c221b3e378
luccastjg <lucasaraujotjg@gmail.com>
2021-03-31 08:47:17 -0300

    começo do sistema de login

---
+++ auth.js
@@ -0,0 +1,11 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBXdg5uwhou0FAKWexUNMzFlx_fZGOCRBA",
+    authDomain: "sistema-de-login-e3685.firebaseapp.com",
+    projectId: "sistema-de-login-e3685",
+    storageBucket: "sistema-de-login-e3685.appspot.com",
+    messagingSenderId: "598024742861",
+    appId: "1:598024742861:web:645fa6a47440147c700684"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
