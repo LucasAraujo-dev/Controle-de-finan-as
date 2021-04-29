240ab64009dbb3bcc743bb0ed80308e77700fced
luccastjg <lucasaraujotjg@gmail.com>
2021-04-29 11:32:11 -0300

    atualizado design e sistema de login

--- auth.js
+++ auth.js
@@ -7,7 +7,8 @@
     projectId: "myproj-auth",
     storageBucket: "myproj-auth.appspot.com",
     messagingSenderId: "276881009769",
-    appId: "1:276881009769:web:638154cb5ef4f8743cc6f8"
+    appId: "1:276881009769:web:638154cb5ef4f8743cc6f8",
+    lang: "pt-br"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
